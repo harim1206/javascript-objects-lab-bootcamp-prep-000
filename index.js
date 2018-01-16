48911ebb675a87ef5fdd53c0823478bf55f623f8
Harim Choi <harim1206@gmail.com>
2018-01-16 21:03:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(obj,key,val){
+  obj[key]=val
+}
+function deleteFromObjectByKey
+function destructivelyUpdateObjectWithKeyAndValue
+function destructivelyDeleteFromObjectByKey
