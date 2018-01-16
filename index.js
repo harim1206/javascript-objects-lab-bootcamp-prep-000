e9457ceda5d2ef6aa990a5c30ae8fa62ed259649
Harim Choi <harim1206@gmail.com>
2018-01-16 21:07:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(obj,key,val){
-  var newObj = Object.assign(obj,{key: val})
+  var newObj = Object.assign(obj,{[key]: val})
   return newObj
 }
+
+
 function deleteFromObjectByKey(obj,key){
   delete obj[key]
   return obj
